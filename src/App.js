5032c8ffdb7a71656058f4c39e14de3da4265004
Tom Quinn <tomquinn@hotmail.co.nz>
2019-05-06 01:12:06 +1200

    add the components to the main app for display (task 2)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
 
   render = () => {
     return (
       <div className="App">
-  
+        <UserInput />
+        <UserOutput></UserOutput>
+        <UserOutput></UserOutput>
       </div>
     );
   }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -3,8 +3,20 @@ import React from 'react';
 const userOutput = (props) => {
     return (
         <div className="UserOutput">
-            <p></p>
-            <p></p>
+            <p>
+                Lorem ipsum commodo sollicitudin erat ornare justo 
+                curabitur integer etiam interdum platea eget, risus
+                volutpat ac mi posuere euismod cursus congue amet 
+                senectus elit, tortor mauris nullam ad ligula 
+                habitasse diam lacus malesuada ac metus.
+            </p>
+            <p>
+                Eu pellentesque sit habitant volutpat dolor blandit 
+                at posuere leo, sagittis enim sollicitudin a per 
+                curae tempus facilisis tempus, cras senectus est 
+                sollicitudin cras venenatis enim nullam massa 
+                quisque id faucibus viverra interdum.
+            </p>
         </div>
     );
 };
